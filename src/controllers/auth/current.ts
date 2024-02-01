4477b57cbb393e78da18f7cc72348b4d8bf6526e
Ihor Karen <karenihor@gmail.com>
2024-02-01 18:55:32 +0200

    current user response

--- src/controllers/auth/current.ts
+++ src/controllers/auth/current.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import { IUser } from "../../models/user/user";
 
 const getCurrent = async (req: Request, res: Response) => {
-  const { email, firstName, lastName, token }: IUser = req.user as IUser;
+  const { email, firstName, lastName, accountType, membership, token }: IUser =
+    req.user as IUser;
 
   res.json({
     email,
     firstName,
     lastName,
+    accountType,
+    membership,
     token,
   });
 };
